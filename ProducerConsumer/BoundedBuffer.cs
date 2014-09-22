029346ee0fa2424068da47c0dd3c945c51e18d32
ates1 <fatihkaraman1907@gmail.com>
2014-09-22 11:37:50 +0200

    it workkkkssssss

---
+++ ProducerConsumer/BoundedBuffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class BoundedBuffer
+    {
+        private Queue<int> _queue;
+        private int _cap;
+
+       
+        public BoundedBuffer(int capacity)
+        {
+            this._cap = capacity;
+            this._queue = new Queue<int>();
+        }
+
+        public bool IsFull()
+        {
+            if (this._queue.Count >= this._cap)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            return true;
+        }
+
+
+
+
+    }
+}
